d28a20d921957ae1316411511e11acc10c4c19b3
TeoKuo <kuosmanen.teo@gmail.com>
2025-04-06 18:01:05 +0300

    part3 3.11 done

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
 
 morgan.token('post-body',(req) =>{
     return req.method === 'POST' ? JSON.stringify(req.body) : '';
@@ -103,7 +104,7 @@ app.post('/api/persons', (request, response) =>{
     response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () =>{
     console.log(`Server running on port ${PORT}`)
 })
